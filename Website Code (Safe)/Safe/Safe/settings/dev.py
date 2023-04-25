ba0e7788bad7be865f782943c87efc1b1b2d0e1d
hanggaa <hanggaprihadi@gmail.com>
2023-04-25 16:15:32 +0700

    Final Safe Paper

---
+++ Website Code (Safe)/Safe/Safe/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '5djeec=kiv5m21qve2nu90s*7id)3h&o#jm3wh_8)g-5_^zre('
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
